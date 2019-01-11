1339b2083751b7933c69d1c744758ba8dfcb0067
mwidmann <widmann.martin@gmail.com>
2019-01-11 14:25:42 +0100

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <the-wrapper>
+    <div slot-scope="{ val, mutate }">
+      <div>{{ val }}</div>
+      <div><button type="button" @click="mutate">+1 💫</button></div>
+      <slot></slot>
+    </div>
+  </the-wrapper>
+</template>
+
+<script>
+import Wrapper from "./Wrapper";
+export default {
+  name: "HelloWorld",
+  components: {
+    TheWrapper: Wrapper
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Wrapper.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  data() {
+    return {
+      mutate: () => {
+        this.val = this.val + 1;
+      },
+      val: 0
+    };
+  },
+  render() {
+    return this.$scopedSlots.default({
+      mutate: this.mutate,
+      val: this.val
+    });
+  }
+};
+</script>
